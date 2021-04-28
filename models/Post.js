b35adbca2ffeda221326d55b3b56f5bd6e79b2e5
Sarvagya Sharma <sarvagya2545@gmail.com>
2021-04-28 10:40:07 +0530

    add description and publish post

--- models/Post.js
+++ models/Post.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const postSchema = mongoose.Schema({
     title: String,
+    description: String,
     content: String,
 });
 
--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const hljs = require('highlight.js');
+const md = require('markdown-it')({
+    html: true,
+    linkify: true,
+    typographer: true,
+    breaks: true,
+    highlight: function (str, lang) {
+        if (lang && hljs.getLanguage(lang)) {
+          try {
+            return hljs.highlight(lang, str).value;
+          } catch (__) {}
+        }
+    
+        return ''; // use external default escaping
+      }
+});
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -15,13 +31,25 @@ mongoose.connect('mongodb://localhost:27017/blog', { useNewUrlParser: true, useU
 app.get('/', async (req,res) => {
     try {
         const posts = await Post.find({});
+        console.log(posts);
+
+        let formattedPosts = posts.map(post => {
+
+            const result = md.render(post.content);
+            return {
+                title: post.title,
+                description: post.description,
+                content: result
+            }
+        })
+
         const links = [
             {
                 name: 'Add post',
                 route: '/new'
             }
         ]
-        res.render('home', { posts, links, title: 'Blog posts' });
+        res.render('home', { posts: formattedPosts, links, title: 'Blog posts' });
     } catch (error) {
         console.log(error)
         res.send(error);
@@ -45,9 +73,21 @@ app.get('/new', async (req,res) => {
 
 app.post('/new', async(req,res) => {
     try {        
-        // const { title, content } = req.body;
+        const { title, content, description } = req.body;
+        const newPost = new Post({
+            title,
+            content,
+            description
+        });
 
-        console.log('body', req.body);
+        newPost.save()
+            .then(result => {
+                console.log(result);
+                res.redirect('/');
+            })
+            .catch(err => {
+                console.log(err);
+            });
 
     } catch (error) {
         console.log(error)
